5c9099a9f86972be91e8ea3bc119f76ec65456b7
coll <2075006513@qq.com>
2023-04-19 08:39:49 +0800

    //校园BBS接口

---
+++ src/test/java/com/nova/zanojishi/ZanojishiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nova.zanojishi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZanojishiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
